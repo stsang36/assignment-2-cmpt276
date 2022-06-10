1a0dbd84d421ae464aef165262a3a1f229372298
Steven Tsang <steventsang@Stevens-MacBook-Pro-2.local>
2022-06-09 22:28:54 -0700

    working database - student_table and display page

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const PORT = process.env.PORT || 5000;
 const { Pool } = require('pg');
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  //ssl: {
-    //rejectUnauthorized: false
+  ssl: {
+    rejectUnauthorized: false
 
-  //}
+  }
 });
 
 app = express()
