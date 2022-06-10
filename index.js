ee4815315f807e4c251b9298751f8091c194e0c6
Steven Tsang <steventsang@Stevens-MacBook-Pro-2.local>
2022-06-09 21:13:35 -0700

    some nodejs stuff wont work

--- index.js
+++ index.js
@@ -18,6 +18,18 @@ app.set('view engine', 'ejs')
 app.get('/', (req, res) => res.render('pages/index'))
 app.get('/histogram', (req, res) => res.render('pages/histogram'))
 app.get('/form', (req, res) => res.render('pages/form'))
+app.get('/display', async (req, res) => {
+  try {
+    const client = await pool.connect();
+    const data = await client.query('SELECT * FROM data');
+    res.render('pages/display', { data: data.rows })
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+});
+
 app.get('/db', async (req, res) => {
   try {
     const client = await pool.connect();
@@ -32,5 +44,23 @@ app.get('/db', async (req, res) => {
 })
 
 app.post('/changeuser', async (req, res) => {
+  try{
+    let name = req.body.serv_name;
+    let weight = req.body.serv_weight;
+    let height = req.body.serv_height;
+    let hairColor = req.body.serv_hColor;
+    let gpa = req.body.serv_gpa;
+
+    const client = await pool.connect();
+    const result = await client.query('INSERT INTO student_table(name, weight, height, hairColor, gpa) VALUES($1, $2, $3, $4, $5)', [name, weight, height, hairColor, gpa]);
+    const results = { 'results': (result) ? result.rows : null};
+    client.release();
+
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+
+  res.send('bruh');
 });
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
